71bc18d8ffb879137034ea0ca6f32316b18a6fd9
Rômulo Junior <132522416+parrylt@users.noreply.github.com>
2023-09-14 00:14:54 -0300

    Add files via upload

---
+++ javascript/script.js
@@ -0,0 +1,35 @@
+/* var tab = 0;
+for (num = 0; num <= 10; num++){
+    document.write (tab + "x" + num + "=" + tab*num + "<br>")
+
+    /*for (tab = 1 ; tab <=10; tab++) {
+        document.write (tab + "x" + num + "=" + tab*num + "<br>");
+    }
+    document.write ("<br>")
+
+    if (num==10){
+        tab++;
+        num=0;
+    }
+    if (tab==11){
+        num=11;
+    }
+}*/
+
+var rep = prompt ("1 - Carros, 2 - Flores, 3 - Casa, 4 - Avião. Digite um número")
+
+if (rep==1){
+    document.write ("<table border> <tr><td><img src='../Aula 5/img/carro.jpg' width='300px' height='200px'></td> <td><img src='../Aula 5/img/carro.jpg' width='300px' height='200px'></td></tr> <tr><td><img src='../Aula 5/img/carro.jpg' width='300px' height='200px'></td> <td><img src='../Aula 5/img/carro.jpg' width='300px' height='200px'></td></tr> <tr><td><img src='../Aula 5/img/carro.jpg' width='300px' height='200 px'></td> <td><img src='../Aula 5/img/carro.jpg' width='300px' height='200 px'></td></tr> </table>");
+}
+else if (rep==2){
+    document.write ("<table border> <tr><td><img src='../Aula 5/img/flores.avif' width='300px' height='200px'></td> <td><img src='../Aula 5/img/flores.avif' width='300px' height='200px'></td></tr> <tr><td><img src='../Aula 5/img/flores.avif' width='300px' height='200px'></td> <td><img src='../Aula 5/img/flores.avif' width='300px' height='200px'></td></tr> <tr><td><img src='../Aula 5/img/flores.avif' width='300px' height='200 px'></td> <td><img src='../Aula 5/img/flores.avif' width='300px' height='200 px'></td></tr> </table>");
+}
+else if (rep==3){
+    document.write ("<table border> <tr><td><img src='../Aula 5/img/casa.jpg' width='300px' height='200px'></td> <td><img src='../Aula 5/img/casa.jpg' width='300px' height='200px'></td></tr> <tr><td><img src='../Aula 5/img/casa.jpg' width='300px' height='200px'></td> <td><img src='../Aula 5/img/casa.jpg' width='300px' height='200px'></td></tr> <tr><td><img src='../Aula 5/img/casa.jpg' width='300px' height='200 px'></td> <td><img src='../Aula 5/img/casa.jpg' width='300px' height='200 px'></td></tr> </table>");
+}
+else if (rep==4){
+    document.write ("<table border> <tr><td><img src='../Aula 5/img/aviao.jpg' width='300px' height='200px'></td> <td><img src='../Aula 5/img/aviao.jpg' width='300px' height='200px'></td></tr> <tr><td><img src='../Aula 5/img/aviao.jpg' width='300px' height='200px'></td> <td><img src='../Aula 5/img/aviao.jpg' width='300px' height='200px'></td></tr> <tr><td><img src='../Aula 5/img/aviao.jpg' width='300px' height='200 px'></td> <td><img src='../Aula 5/img/aviao.jpg' width='300px' height='200 px'></td></tr> </table>");
+}
+else{
+    document.write ("valor inválido");
+}
